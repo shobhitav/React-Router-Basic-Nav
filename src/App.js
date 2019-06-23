1a4e3c5a834590db8905578d3bc4e59cab8bdd9e
Shobhita Vaishnav <to.shobhita@gmail.com>
2019-06-23 11:36:25 -0700

    Further Fix

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
 import React from 'react';
 import './App.css';
 import { Home, About, Contact, Navigation } from './components';
+import {BrowserRouter as Router,Route,Link } from "react-router-dom" ;
 
 const App = () => (
+  <Router>
   <div>
     <Navigation />
+  
+  <Route path="/" component={Home} exact />
+  <Route path="/about" exact component={About} />
+  <Route path="/contact" component={Contact} />
   </div>
+  </Router>
 );
 
 export default App;
